cae8811c2c705ca4b6f39cefb20aee14d181ae68
Jannik Beyerstedt <beyerstedt@consider-it.de>
2021-07-19 14:48:01 +0200

    MQTT-Replay: Add some sleep to the timing loop
    Evaluating the elapsed time in a for loop without letting the thread sleep for a
    while is a bad practice and causes 100% CPU load. The message time
    stamps have a resolution of ms, so sleeping 200 us should create a
    replay timing, which is accurate enough.

--- mqtt-replay/main.go
+++ mqtt-replay/main.go
@@ -126,6 +126,7 @@ func mqtt_replay(filename, url string) {
 				if (millis() - t0) >= (msg.Millis - t1) {
 					break
 				}
+				time.Sleep(200 * time.Microsecond)
 			}
 		}
 		fmt.Printf("mqtt_replay() : t=%d topic=%s, payload_size=%d\n", msg.Millis, msg.Topic, payload_size)
